b64646850f7e1ba2b4870a29aea851daf633157d
JohanZhu <634606335@qq.com>
2016-09-13 21:32:02 +0800

    finished version

--- 弹幕墙.js
+++ 弹幕墙.js
@@ -1,8 +1,10 @@
 $(document).ready(function(){
-	/*引入野狗*/
+/*引入野狗、用本地存储内容进行渲染*/
 	var ref =  new Wilddog("https://johandanmu.wilddogio.com/");
     $('.bullet').append(localStorage.getItem('danmu'));
-	/*给发射！添加点击事件 */
+    
+    
+/*给发射！添加点击事件 */
 	$(".shoot").click(function(){
 		/*点击后把数据提交给野狗*/
 		var text = $(".text").val();
@@ -28,33 +30,73 @@ $(document).ready(function(){
 			videoSecond = '0'+videoSecond;
 		}
 		var VTime = videoMinute+':'+videoSecond;
-		/*定义本地存储来存储弹幕的DOM*/
+		
+		
+/*创建历史弹幕记录（本地存储）*/
 		localStorage.danmu = '<div class="history"><div class="time">'+VTime+'</div><div class="content">'+ text +'</div><div class="date">'+month+"-"+day+' '+hour+':'+minute+'</div></div>';
 		$('.bullet').append(localStorage.getItem('danmu'));
 		localStorage.danmu = $('.bullet').html();
 		/*创建视频中的弹幕*/
 		var randomTop = RandomNum(20,170);
-		console.log(randomTop);
-		$('<div class="floatBullet" style="top:'+randomTop+'px'+'">'+text+'</div>').appendTo($('.border'));
+		var instantBullet = '<div class="floatBullet" style="top:'+randomTop+'px'+'">'+text+'</div>';
+		
+		$(instantBullet).appendTo($('.border'));
 		/*清空输入框*/
 		$('.text').val("");
 	});
-	/*给input框添加回车事件，按回车等于点击！*/
+	
+	
+/*给input框添加回车事件，按回车等于点击！*/
 	$(".text").keypress(function(event){
 		/*键盘按下时，判断是否是回车，若是则触发发射的点击事件*/
 		if(event.keyCode == "13"){
 			$('.shoot').trigger('click');
 		}
 	});
-	/*定义清除*/
+/*定义清屏按钮，点击后清除所有的本地存储*/
 	$('.clean').click(function(){
 		$('.history').html('');
 		window.localStorage.clear();
 	});
-	/*待不全功能：历史弹幕，按日期显示弹幕，过长弹幕hover出现提示框*/
+	
+/*获取弹幕发送时间数组与弹幕内容数组（取自存储记录）*/
+    var delayTimeArr = [];
+    var contentArr = [];
+    var historyTime = $('.bullet').find('.time');
+	var nodeContent = $('.bullet').find('.content');
+	for(var i = 0;i < historyTime.length;i++){
+			 var timeArr = historyTime[i].innerHTML;
+			 var contentText = nodeContent[i].innerHTML;
+			 var tenMinute = parseInt(timeArr.charAt(0));
+			 var oneMinute = parseInt(timeArr.charAt(1));
+			 var tenSecond = parseInt(timeArr.charAt(3));
+			 var oneSecond = parseInt(timeArr.charAt(4));
+			 var delayTime = ((tenMinute * 10 + oneMinute) * 60 +(tenSecond * 10 + oneSecond))*1000;
+			 delayTimeArr.push(delayTime);
+			 contentArr.push(contentText);
+	    }
+	/*把两个数组结果打印出来看是否出错*/
+	     console.log(delayTimeArr);
+	     console.log(contentArr);
+         var randomTop = RandomNum(20,90);
+/*给播放加监听事件，当开始播放后执行下面事件 = 根据存储的历史弹幕来发射屏幕上的弹幕*/     
+	$('video')[0].addEventListener('play',function(){
+		for(var i = 0;i < delayTimeArr.length;i++){
+	    	var  floatHistory = '<div class="floatBullet" style="top:'+randomTop+'px'+'">'+contentArr[i]+'</div>';
+	    	console.log(floatHistory);
+	    	/*延迟发射弹幕，延迟时间与弹幕内容读取自本地存储，也久是上述数组取的内容*/
+	    	setTimeout(function(){
+	    		$(floatHistory).appendTo($('.border'));
+	    	},delayTimeArr[i]);
+		}   
+	});
+	
+/*显示过长弹幕*/	
+//$('.content').append('<div class="showAll">测试内容</div>')
 	
 	
-	/*取范围内的数字（用于弹幕的随机高度）*/
+			        
+/*取范围内的数字（用于弹幕的随机高度）*/
 function RandomNum(Min,Max){
 var Range = Max - Min;
 var Rand = Math.random();   
